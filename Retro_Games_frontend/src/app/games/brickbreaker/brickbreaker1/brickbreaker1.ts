a295943ec36aadfc0d5b50752bd4f08ebe9918ee
Cosereanu Emanuel <77057756+emanuelco07@users.noreply.github.com>
2026-05-20 17:58:52 +0300

    changin colors for paddles, hearts, bricks and ball in brickbreaker level 1

--- Retro_Games_frontend/src/app/games/brickbreaker/brickbreaker1/brickbreaker1.ts
+++ Retro_Games_frontend/src/app/games/brickbreaker/brickbreaker1/brickbreaker1.ts
@@ -54,10 +54,10 @@ export class Brickbreaker1 {
 
   //definim culorile pentru fiecare piesa
   pieceColor: { [key: string]: string} = {
-    Paddle: '#a86cf5',
-    Ball: '#40e0d0',
-    Bricks: '#ff6fa1',
-    Life: 'red'
+    Paddle: '#00eaff',
+    Ball: '#ff2ec4',
+    Bricks: '#1552d1',
+    Life: '#ff3b3b'
   };
 
   playing = false; //variabila care ne ajuta sa stim daca jocul este in desfasurare sau nu
