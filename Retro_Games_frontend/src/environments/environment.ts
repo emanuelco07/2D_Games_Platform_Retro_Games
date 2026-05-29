e40d5e845eab1aae6abdafcaa3a6c1b31f00144e
Cosereanu Emanuel <77057756+emanuelco07@users.noreply.github.com>
2026-05-29 20:03:28 +0300

    adding rootDir in frontend

--- Retro_Games_frontend/src/environments/environment.ts
+++ Retro_Games_frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5062/api' // URL ul API ul ASP.NET Core (practic aici gasim backend ul)
+  apiUrl: 'http://localhost:5000/api' // URL ul API ul ASP.NET Core (practic aici gasim backend ul)
 };
